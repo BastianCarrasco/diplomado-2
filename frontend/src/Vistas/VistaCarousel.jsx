1955aa6a0c903cd470481f87943c2e24df8c6ab0
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-04-11 02:50:14 -0400

    nuevos vistas

--- frontend/src/Vistas/VistaCarousel.jsx
+++ frontend/src/Vistas/VistaCarousel.jsx
@@ -12,51 +12,6 @@ const VistaCarousel = ({ src, alt, isActive }) => {
         className="d-block w-full h-auto max-h-screen object-cover"
         alt={alt}
       />
-
-      {/* Logo superior izquierdo */}
-      <div className="absolute top-4 left-4 z-10 max-w-xs max-h-48 sm:max-w-xs sm:max-h-24">
-        <img
-          src= {logo} // Asegúrate de que la ruta de la imagen sea correcta
-          alt="Logo"
-          className="img-fluid max-h-full object-contain"
-        />
-      </div>
-
-      {/* Logo superior derecho */}
-      <div className="absolute top-4 right-4 z-10 max-w-xs max-h-48 sm:max-w-xs sm:max-h-24">
-        <p className="text-white text-xl font-bold sm:text-lg">
-          cii.pucv.cl
-        </p>
-      </div>
-
-      {/* Logo inferior izquierdo */}
-      <div className="absolute bottom-4 left-4 z-10 max-w-xs max-h-48 sm:max-w-xs sm:max-h-24">
-        <img
-          src={logo2}
-          alt="Logo"
-          className="img-fluid max-h-full object-contain"
-        />
-      </div>
-
-      {/* Logo inferior derecho */}
-      <div className="absolute bottom-4 right-4 z-10 max-w-xs max-h-48 sm:max-w-xs sm:max-h-24">
-        <img
-          src={logo3}
-          alt="Logo"
-          className="img-fluid max-h-full object-contain"
-        />
-      </div>
-
-      {/* Texto centrado vertical y horizontalmente */}
-      <div
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center p-4 bg-black bg-opacity-50 rounded-lg border-r-4 border-b-4 border-yellow-500 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl"
-      >
-        <h2 className="text-white text-2xl md:text-4xl font-monserrat uppercase leading-tight sm:text-xl">
-          Diplomado <br />
-          Internacional <br />
-          en innovación
-        </h2>
-      </div>
     </div>
   );
 };
--- frontend/src/Vistas/VistaCarousel2.jsx
+++ frontend/src/Vistas/VistaCarousel2.jsx
@@ -2,75 +2,14 @@ import React from 'react';
 
 const VistaCarousel2 = ({ src, alt, isActive }) => {
   return (
-    <div
-    className={`carousel-item position-relative font-sans ${isActive ? 'active' : ''}`}
-    >
-      <div className="grid grid-cols-1 md:grid-cols-2 h-screen">
-        {/* Columna izquierda - Imagen a pantalla completa */}
-        <div className="col-span-1 md:col-span-1 h-full relative">
-          <img
-            src={src}
-            className="absolute top-0 left-0 w-full h-full object-cover"
-            style={{ objectPosition: 'center center' }} // Ajusta la imagen para centrarse en todas las resoluciones
-            alt={alt}
-          />
-        </div>
-
-        {/* Columna derecha - Contenido estructurado */}
-        <div
-          className="col-span-1 md:col-span-1 h-full bg-white flex items-center border-l-8 border-gray-700 p-4 md:p-8"
-        >
-          <div  className="w-full">
-            <h3
-              className="text-dark font-bold mb-4 text-center md:text-left px-2 py-2"
-              style={{
-                backgroundColor: 'hwb(52 0% 5% / 0.981)',
-                display: 'inline-block',
-                borderRight: '4px solid black',
-                borderBottom: '4px solid black',
-                padding: '5px 10px',
-                font: 'monserrat',
-              }}
-            >
-              CONOCIMIENTOS ESPERADOS
-            </h3>
-            <ol className="text-dark list-decimal pl-6">
-              <li className="mb-3">
-                <strong>Comprensión integral de la innovación:</strong><br />
-                Desarrollar una visión global del proceso innovador, desde la
-                generación de ideas hasta su implementación, comprendiendo cómo la
-                innovación impulsa la competitividad y transforma organizaciones y
-                sectores.
-              </li>
-              <li className="mb-3">
-                <strong>Adquisición de marcos teóricos y herramientas aplicadas de
-                  innovación:</strong
-                ><br />
-                Incorpora modelos reconocidos y metodologías prácticas para
-                gestionar la innovación, aplicando herramientas como Design
-                Thinking, Lean Startup, entre otros en contextos reales.
-              </li>
-              <li className="mb-3">
-                <strong>Capacidad para diseñar estrategias de innovación
-                  organizacional:</strong
-                ><br />
-                Aprende a identificar oportunidades, formular propuestas y liderar
-                procesos de cambio, desarrollando estrategias alineadas a los
-                objetivos y capacidades de su organización.
-              </li>
-              <li>
-                <strong>Conocimiento de experiencias y buenas prácticas
-                  internacionales:</strong
-                ><br />
-                Conocer casos exitosos de empresas líderes a nivel global en
-                innovación, a través de referentes internacionales y aprendizajes
-                del entorno.
-              </li>
-            </ol>
-          </div>
-        </div>
-      </div>
-    </div>
+    <div className={`carousel-item ${isActive ? 'active' : ''}`}>
+    {/* Imagen de fondo */}
+    <img
+      src={src}
+      className="d-block w-full h-auto max-h-screen object-cover"
+      alt={alt}
+    />
+  </div>
   );
 };
 
